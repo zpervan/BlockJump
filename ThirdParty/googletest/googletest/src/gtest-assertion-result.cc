9e9b7d231280e460d13eabecc1a18ffbd154b042
Zvonimir Pervan <z.pervan65@gmail.com>
2022-06-29 19:06:42 +0200

    Update googletest library

--- ThirdParty/googletest/googletest/src/gtest-assertion-result.cc
+++ ThirdParty/googletest/googletest/src/gtest-assertion-result.cc
@@ -27,27 +27,51 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-// A sample program demonstrating using Google C++ testing framework.
-#ifndef GOOGLETEST_SAMPLES_SAMPLE4_H_
-#define GOOGLETEST_SAMPLES_SAMPLE4_H_
+// The Google C++ Testing and Mocking Framework (Google Test)
+//
+// This file defines the AssertionResult type.
 
-// A simple monotonic counter.
-class Counter {
- private:
-  int counter_;
+#include "gtest/gtest-assertion-result.h"
 
- public:
-  // Creates a counter that starts at 0.
-  Counter() : counter_(0) {}
+#include <string>
+#include <utility>
 
-  // Returns the current counter value, and increments it.
-  int Increment();
+#include "gtest/gtest-message.h"
 
-  // Returns the current counter value, and decrements it.
-  int Decrement();
+namespace testing {
 
-  // Prints the current counter value to STDOUT.
-  void Print() const;
-};
+// AssertionResult constructors.
+// Used in EXPECT_TRUE/FALSE(assertion_result).
+AssertionResult::AssertionResult(const AssertionResult& other)
+    : success_(other.success_),
+      message_(other.message_.get() != nullptr
+                   ? new ::std::string(*other.message_)
+                   : static_cast< ::std::string*>(nullptr)) {}
 
-#endif  // GOOGLETEST_SAMPLES_SAMPLE4_H_
+// Swaps two AssertionResults.
+void AssertionResult::swap(AssertionResult& other) {
+  using std::swap;
+  swap(success_, other.success_);
+  swap(message_, other.message_);
+}
+
+// Returns the assertion's negation. Used with EXPECT/ASSERT_FALSE.
+AssertionResult AssertionResult::operator!() const {
+  AssertionResult negation(!success_);
+  if (message_.get() != nullptr) negation << *message_;
+  return negation;
+}
+
+// Makes a successful assertion result.
+AssertionResult AssertionSuccess() { return AssertionResult(true); }
+
+// Makes a failed assertion result.
+AssertionResult AssertionFailure() { return AssertionResult(false); }
+
+// Makes a failed assertion result with the given failure message.
+// Deprecated; use AssertionFailure() << message.
+AssertionResult AssertionFailure(const Message& message) {
+  return AssertionFailure() << message;
+}
+
+}  // namespace testing
