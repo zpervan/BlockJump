b45bfc72227383fc7fc52da59829b011e28cf6e9
Zvonimir Pervan <z.pervan65@gmail.com>
2022-12-30 16:29:50 +0100

    Add server skeleton functionality

--- Server/main.go
+++ Server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"server/proto"
+)
 
 func main() {
-    fmt.Println("Hello from the server")
+	fmt.Println("starting game server")
+
+	listener, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		panic(err)
+	}
+
+	server := grpc.NewServer()
+	proto.RegisterGrpcServer(server, &proto.GameServer{})
+	if err := server.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
