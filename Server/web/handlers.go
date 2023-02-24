2f527e521870642e8f423c7d12fc6af7ae07a898
Zvonimir Pervan <z.pervan65@gmail.com>
2023-02-24 12:46:44 +0100

    Adjust server game creation and listing

--- Server/web/handlers.go
+++ Server/web/handlers.go
@@ -9,17 +9,36 @@ import (
 type GameServer struct {
 	logger *core.Logger
 	proto.UnimplementedBlockJumpServer
+	listOfGames *proto.ListOfGames
 }
 
-func NewServer(logger *core.Logger) *GameServer{
+func NewServer(logger *core.Logger) *GameServer {
 	server := &GameServer{}
 
 	server.logger = logger
+	server.listOfGames = &proto.ListOfGames{}
 
 	return server
 }
 
 func (gs *GameServer) TestConnection(_ context.Context, in *proto.DummyRequest) (*proto.DummyResponse, error) {
-	gs.logger.Info("received request from client")
+	gs.logger.Info("received test connection request from client")
+
+	if in.DummyData == "" {
+		return &proto.DummyResponse{}, nil
+	}
+
 	return &proto.DummyResponse{DummyData: "received following data - " + in.DummyData}, nil
+}
+
+func (gs *GameServer) CreateGame(_ context.Context, in *proto.Game) (*proto.Empty, error) {
+    // todo: Add some input data validation
+    gs.logger.Info("creating new game")
+    gs.listOfGames.AvailableGames = append(gs.listOfGames.AvailableGames, in)
+    return &proto.Empty{}, nil
+}
+
+func (gs *GameServer) ListAllGames(_ context.Context, _ *proto.Empty) (*proto.ListOfGames, error) {
+    gs.logger.Info("fetching list of all games")
+    return gs.listOfGames, nil
 }
